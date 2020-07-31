e3c42d41ee82700e1decae60a9f676677a9721a2
bavitra-sruthi <bavitrasruthi45@gmail.com>
2020-07-31 13:43:36 +0530

    updated jwt

--- src/main/java/com/tgt/strikers/Service/JwtUserDetailsService.java
+++ src/main/java/com/tgt/strikers/Service/JwtUserDetailsService.java
@@ -25,16 +25,16 @@ public class JwtUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        if ("site admin".equals(username)) {
-//            return new User("site admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
-//                    new ArrayList<>());
+        if ("Site Admin".equals(username)) {
+            return new User("Site Admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+      }
+//        Optional<Admin> optionalAdmin = adminRepository.findById(username);
+//        if(optionalAdmin.isPresent()) {
+//            Admin temp = optionalAdmin.get();
+//
+//            return new User(temp.getAdmin_firstname(), "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", new ArrayList<>());
 //        }
-        Optional<Admin> optionalAdmin = adminRepository.findById(username);
-        if(optionalAdmin.isPresent()) {
-            Admin temp = optionalAdmin.get();
-
-            return new User(temp.getAdmin_firstname(), "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", new ArrayList<>());
-        }
         else {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
